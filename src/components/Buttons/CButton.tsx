9d10f08c4d48622202c407226d54b0f361444aea
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-05-22 20:10:08 -0400

    add CButton

---
+++ src/components/Buttons/CButton.tsx
@@ -0,0 +1,12 @@
+export const CButton = () => {
+  return (
+    <button
+      className="
+        flex justify-between items-center shadow p-2 rounded-full
+        bg-pink-500 text-pink-50
+      "
+    >
+      CButton
+    </button>
+  );
+};
