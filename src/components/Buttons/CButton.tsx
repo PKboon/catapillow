3f7bfac1c5e000d12428f1671c8db6c6bdb799c0
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-05-23 23:24:28 -0400

    add dark mode to CButton

--- src/components/Buttons/CButton.tsx
+++ src/components/Buttons/CButton.tsx
@@ -8,7 +8,7 @@ const styles = cva(
     "items-center",
     "enabled:shadow-md",
     "py-1.5",
-    "px-3",
+    "px-4",
     "rounded-full",
     "font-semibold",
     "text-sm",
@@ -26,36 +26,60 @@ const styles = cva(
           "hover:bg-primaryLight-400",
           "focus:bg-primaryLight-400",
           "active:bg-primaryLight-600",
+          "dark:text-primaryDark-950 dark:bg-primaryDark-400",
+          "dark:hover:bg-primaryDark-300",
+          "dark:focus:bg-primaryDark-300",
+          "dark:active:bg-primaryDark-500",
         ],
         secondary: [
           "text-primaryLight-500 bg-slate-50",
           "hover:text-primaryLight-50 hover:bg-primaryLight-400",
           "focus:text-primaryLight-50 focus:bg-primaryLight-400",
           "active:text-primaryLight-50 active:bg-primaryLight-500",
+          "dark:text-primaryDark-950 dark:bg-slate-300",
+          "dark:hover:bg-primaryDark-300",
+          "dark:focus:bg-primaryDark-300",
+          "dark:active:bg-primaryDark-500",
         ],
         success: [
           "text-success-800 bg-success-100",
           "hover:text-success-50 hover:bg-success-500",
           "focus:text-success-50 focus:bg-success-500",
           "active:text-success-900 active:bg-success-400",
+          "dark:text-success-50 dark:bg-success-700",
+          "dark:hover:text-success-100 dark:hover:bg-success-600",
+          "dark:focus:text-success-100 dark:focus:bg-success-600",
+          "dark:active:text-success-950 dark:active:bg-success-400",
         ],
         danger: [
           "text-danger-700 bg-danger-100",
           "hover:text-danger-50 hover:bg-danger-500",
           "focus:text-danger-50 focus:bg-danger-500",
           "active:text-danger-950 active:bg-danger-400",
+          "dark:text-danger-50 dark:bg-danger-700",
+          "dark:hover:bg-danger-600",
+          "dark:focus:bg-danger-600",
+          "dark:active:bg-danger-500",
         ],
         warning: [
           "text-warning-800 bg-warning-100",
           "hover:text-warning-900 hover:bg-warning-300",
           "focus:text-warning-900 focus:bg-warning-300",
           "active:text-warning-950 active:bg-warning-400",
+          "dark:text-warning-50 dark:bg-warning-600",
+          "dark:hover:text-warning-950 dark:hover:bg-warning-500",
+          "dark:focus:text-warning-950 dark:focus:bg-warning-500",
+          "dark:active:text-warning-950 dark:active:bg-warning-400",
         ],
         info: [
           "text-info-800 bg-info-100",
           "hover:bg-info-300",
           "focus:bg-info-300",
           "active:text-info-50 active:bg-info-400",
+          "dark:text-info-50 dark:bg-info-600",
+          "dark:hover:text-info-950 dark:hover:bg-info-500",
+          "dark:focus:text-info-950 dark:focus:bg-info-500",
+          "dark:active:text-info-950 dark:active:bg-info-400",
         ],
       },
     },
@@ -68,10 +92,10 @@ const styles = cva(
 type CButtonProps = ComponentProps<"button"> & VariantProps<typeof styles>;
 
 export const CButton = forwardRef<HTMLButtonElement, CButtonProps>(
-  ({ color, className, ...props }, ref) => {
+  ({ color, className, children, ...props }, ref) => {
     return (
       <button ref={ref} className={cn(styles({ color, className }))} {...props}>
-        {props.children}
+        {children}
       </button>
     );
   }
