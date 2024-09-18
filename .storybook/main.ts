6baa5624f03f7ca21a3cf75eba5945ccbbdcc3d5
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2024-09-18 00:04:35 -0400

    add stackbliz addon to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,36 +3,39 @@ import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+		"@stackblitz/storybook-addon-stackblitz",
+	],
 
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
 
-  docs: {},
+	docs: {},
 
-  viteFinal: async (config) => {
-    config.plugins?.push(
-      // We need it because we define "paths" in the tsconfig.json file
-      tsconfigPaths({
-        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
-      })
-    );
+	viteFinal: async (config) => {
+		config.plugins?.push(
+			// We need it because we define "paths" in the tsconfig.json file
+			tsconfigPaths({
+				projects: [
+					path.resolve(path.dirname(__dirname), "tsconfig.json"),
+				],
+			})
+		);
 
-    return config;
-  },
+		return config;
+	},
 
-  typescript: {
-    reactDocgen: "react-docgen-typescript"
-  }
+	typescript: {
+		reactDocgen: "react-docgen-typescript",
+	},
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,14 +2,15 @@ import type { Preview } from "@storybook/react";
 import "../src/index.css";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		repositoryUrl: "https://github.com/PKboon/catapillow",
+	},
 };
 
 export default preview;
